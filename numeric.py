3350d79358af3241160c534e442b24cc4e800c12
“jabberwocky0139” <torii0139@asagi.waseda.jp>
2016-11-21 01:32:23 +0900

    ok

---
+++ numeric.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sbn
+
+
+def f(x):
+    return np.exp(-x**2)
+
+L, N = 7, 100
+x = np.linspace(-L/2, L/2, N)
+psi = f(x)
+
+K = np.eye(N, N)
+K_sub = np.vstack((K[1:], np.array([0] * N)))
+K = -2 * K + K_sub + K_sub.T
+
+dx = L/N
+psi_dot = dx**-2 * np.dot(K, psi)
+
+
+plt.plot(x, psi)
+plt.plot(x, psi_dot)
+plt.xlim(-3.5, 3.5)
+plt.ylim(-2.5, 2)
+
+plt.show()
+
+
